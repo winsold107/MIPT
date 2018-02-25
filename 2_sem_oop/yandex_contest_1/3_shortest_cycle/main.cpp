9c34cda1d9e1a1e164fb7803ee5406eedfca5745
winsold107 <dolgova.ad@phystech.edu>
2018-02-25 18:03:07 +0300

    2 problem about shortest cycle solved

--- 2_sem_oop/yandex_contest_1/3_shortest_cycle/main.cpp
+++ 2_sem_oop/yandex_contest_1/3_shortest_cycle/main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+//#include <limits>
 
 using namespace std;
 
-void bfs (vector<vector<int>> g, vector <bool>& used, vector <int>& cycle, int start, int n, vector < int > len) {
-    //vector < int > len (n);
+int ans = 999999999;
 
+void bfs(const vector<vector<int>> &g, vector<int> &cycle, int start, int n) {
+    //vector < int > len (n);
+    vector<int> len(n, 0);
+//    vector<bool> used(n, false);
+    vector<int> from(n, -1);
     queue<int> q;
     q.push(start);
 
-    used[start] = true;
+//    used[start] = true;
 
     while (!q.empty()) {
         int v = q.front();
@@ -19,18 +24,22 @@ void bfs (vector<vector<int>> g, vector <bool>& used, vector <int>& cycle, int s
         for (int i = 0; i < g[v].size(); i++) {
             int to = g[v][i];
 
-            if (!used[to]) {
-                used[to] = true;
+            if (from[v] == to) continue;
+
+            if (from[to] == -1) {
+                from[to] = v;
                 q.push(to);
                 len[to] = len[v] + 1;
-                //cout << len[v];
-            }
-
-            else {
+//                cout << len[v];
+            } else {
                 //cout<<"1 ";
-                cycle[to] = len[to]+1;
-                cout<<cycle[to]<<" ";
+                int len_candidate = len[v] + 1 + len[to];
+                if (ans > len_candidate) {
+                    ans = len_candidate;
+                }
                 return;
+//                cycle[start] = len[v] + 1 + len[to];
+//                cout << cycle[to] << " ";
             }
         }
     }
@@ -43,47 +52,48 @@ int main() {
     int m;
     cin >> m;
 
-    vector <vector < int >> NOGraph(n);
+    vector<vector<int >> NOGraph(n);
 
-    for (int i = 0; i<m; i++) {
+    for (int i = 0; i < m; i++) {
         int beg;
         int end;
         cin >> beg;
         cin >> end;
         NOGraph[beg].push_back(end);
-    }
-    ///кажется некорректно работает чтение графа, прочитать про списки смежностей
-    ///true или же значение соединения (те 2й конец ребра)?
-
-    vector < int > len_cycle (n, 0);
-    vector < bool > used (n, false);
-    vector < int > length (n, 0);
-
-    for (int index = 0; index<n; index++) {
-        bfs(NOGraph, used, len_cycle, index, n, length);
+        NOGraph[end].push_back(beg);
     }
 
-    int minimum = 9999999;
-    int counter = 0;
+    vector<int> len_cycle(n, 0);
 
-    for (unsigned long i = 0; i<n; i++){
-        if (len_cycle[i] < minimum) {
-            if ((len_cycle[i] == 0)||(len_cycle[i] == 1)) {
-                counter += 1;
-            }
-            else if (len_cycle[i]!=1){
-                minimum = len_cycle[i];
-            }
-        }
+    for (int index = 0; index < n; index++) {
+        bfs(NOGraph, len_cycle, index, n);
     }
-    for (int i = 0; i<n; i++){
-        cout<<len_cycle[i]<<" ";
-    }
-    if (counter == n) {
-        cout << "0 ";
+
+//    int minimum = numeric_limits<int>::max();
+//    int counter = 0;
+
+//    for (unsigned long i = 0; i < n; i++) {
+//        if (len_cycle[i] < minimum) {
+//            if ((len_cycle[i] == 0) || (len_cycle[i] == 1)) {
+//                counter += 1;
+//            } else if (len_cycle[i] != 1) {
+//                minimum = len_cycle[i];
+//            }
+//        }
+//    }
+//    for (int i = 0; i < n; i++) {
+//        cout << len_cycle[i] << " ";
+//    }
+//    if (counter == n) {
+//        cout << "0 ";
+//    } else {
+//        cout << minimum;
+//    }
+    if (ans == 999999999) {
+        cout<<"-1";
     }
     else {
-        cout << minimum;
+        cout << ans << endl;
     }
     return 0;
 }
