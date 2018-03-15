98d75085c9ad88559d90402a17da79ba30006f0f
winsold107 <dolgova.ad@phystech.edu>
2018-03-15 11:42:56 +0300

    python problem 7, 4 solved, problem 7 needs refresh

--- 2_sem_python/yandex_contest_3/contest/4_password power.py
+++ 2_sem_python/yandex_contest_3/contest/4_password power.py
@@ -6,61 +6,55 @@ def UnicalSymbols(s):
         else:
             a[s[i]] = s.count(s[i])
     ret = False
-    print(a)
-    print(len(a))
     if len(a) >= 4:
         ret = True
     return ret
 
-# def has_enough_unique_symbols(password):
-#     a = dict()
-#     for c in password:
-#         a[c] = True
-#     return len(a) >= 4
-#
 
-# Условие:
-#
-# Правила простые: сильным может считаться только тот пароль,
-# в котором есть буквы в разных регистрах и цифры.
-#
-# Пароли длины менее 8,
-# пароли состоящие менее чем из 4 уникальных символов,
-# и пароли в которых встречается слово anna (в любом регистре букв) считаются слабыми.
-#
-#
-#
-# Возможно проблема в том, что условие не запрещает использовать что-то кроме букв и цифр.
-# Однако все пароли, в которых нет либо букв, либо цифр - по дефолту должны быть слабыми. (кажется так)
-#
-# >>> 1234567&
-# >>> strong
-#
-# >>> 1234567t
-# >>> weak
+def AnyDigit(s):
+    for i in range(len(s)):
+        if s[i].isdigit():
+            return True
+        else:
+            continue
+    return False
 
-# А еще, возможно потому что, ты считаешь уникальные символы уже в строке, где сделала lowercase
-# Т.е. символы 'А' и 'а' будут одим и тем же символом. Хотя, вроде как, это не так.
-#
-# >>> aA1212121212
-# >>> weak
-#
+
+def AnyLetter(s):
+    for i in range(len(s)):
+        if s[i].isalpha():
+            return True
+        else:
+            continue
+    return False
+
+
+def AnyUpper(s):
+    for i in range(len(s)):
+        if s[i].isupper():
+            return True
+        else:
+            continue
+    return False
+
+
+def AnyLower(s):
+    for i in range(len(s)):
+        if s[i].islower():
+            return True
+        else:
+            continue
+    return False
 
 
 string = str(input())
 strin = string.lower()
-one = string.islower()
-two = string.isupper()
-three = string.isdigit()
-four = string.isalpha()
-five = UnicalSymbols(string)
-six = strin.find("anna") == -1
-seven = len(string) >= 8
-eight = string.isalnum()
-if (not one) and (not two) \
-        and (six) \
-        and (seven) \
-        and (five):
+NoAnna = strin.find("anna")
+Eight = len(string)
+if (AnyUpper(string)) and (AnyLower(string)) and\
+        (AnyDigit(string)) and (AnyLetter(string)) and\
+        (NoAnna == -1) and (Eight >= 8) and\
+        (UnicalSymbols(strin)):
         print("strong")
 else:
     print("weak")
